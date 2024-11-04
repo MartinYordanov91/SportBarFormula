2b781ea91b731bc38b8c7c954f927a178760b4bf
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-04 11:48:19 +0200

    creating and adding a data Constatns  for  Category model

---
+++ SportBarFormula.Infastructure/Constants/DataConstants/CategoryConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace SportBarFormula.Infastructure.Constants.DataConstants;
+
+public static class CategoryConstants
+{
+    public const int CategoryNameMinLength = 3;
+    public const int CategoryNameMaxLength = 50;
+
+}
