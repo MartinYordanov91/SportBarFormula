e481cbdd2981cffad4e37bea2628369dfaa0abe9
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-04 11:47:48 +0200

    creating and adding a data model Category

---
+++ SportBarFormula.Infastructure/Data/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static SportBarFormula.Infastructure.Constants.DataConstants.CategoryConstants;
+
+namespace SportBarFormula.Infastructure.Data.Models;
+
+public class Category
+{
+    [Key]
+    [Comment("Unique identifier of the Category")]
+    public int CategoryId { get; set; }
+
+    [MaxLength(CategoryNameMaxLength)]
+    [Comment("Category name (drinks, pizzas)")]
+    public required string Name { get; set; }
+}
