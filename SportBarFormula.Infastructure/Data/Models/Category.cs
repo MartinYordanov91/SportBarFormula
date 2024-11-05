b584b9aa042df95ddfc3ec53f68d0a276147cd88
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-06 00:02:13 +0200

    added virtoal Icollection MenuItem to category

--- SportBarFormula.Infastructure/Data/Models/Category.cs
+++ SportBarFormula.Infastructure/Data/Models/Category.cs
@@ -4,8 +4,14 @@ using static SportBarFormula.Infastructure.Constants.DataConstants.CategoryConst
 
 namespace SportBarFormula.Infastructure.Data.Models;
 
+[Comment("Group menu items by categories such as drinks, main courses, desserts.")]
 public class Category
 {
+    public Category()
+    {
+        this.MenuItems = new HashSet<MenuItem>();
+    }
+
     [Key]
     [Comment("Unique identifier of the Category")]
     public int CategoryId { get; set; }
@@ -13,4 +19,8 @@ public class Category
     [MaxLength(CategoryNameMaxLength)]
     [Comment("Category name (drinks, pizzas)")]
     public required string Name { get; set; }
+
+
+    [Comment("Collection of menu items associated with this category")]
+    public virtual ICollection<MenuItem> MenuItems { get; set; }
 }
