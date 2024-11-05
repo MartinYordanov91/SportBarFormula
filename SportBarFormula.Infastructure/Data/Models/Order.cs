32d84a4c1a97824013e71e9150bfed5ed3ceedcb
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-06 00:03:20 +0200

    rename

---
+++ SportBarFormula.Infastructure/Data/Models/Order.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static SportBarFormula.Infastructure.Constants.DataConstants.OrdersConstants;
+
+namespace SportBarFormula.Infastructure.Data.Models;
+
+[Comment("Contains information about orders placed by customers.")]
+public class Order
+{
+    public Order()
+    {
+        this.OrderItems = new HashSet<OrderItem>();
+    }
+
+    [Key]
+    [Comment("Unique identifier of the order")]
+    public int OrderId { get; set; }
+
+    [Comment("Identifier of the user who placed the order")]
+    public required string UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public required virtual IdentityUser User { get; set; }
+
+    [Comment("Order date")]
+    public required DateTime OrderDate { get; set; }
+
+    [Column(TypeName = OrderTotalAmountPrecision)]
+    [Comment("Total amount of the order")]
+    public required decimal TotalAmount { get; set; }
+
+    [Comment("Collection of order items")]
+    public virtual ICollection<OrderItem> OrderItems { get; set; }
+}
