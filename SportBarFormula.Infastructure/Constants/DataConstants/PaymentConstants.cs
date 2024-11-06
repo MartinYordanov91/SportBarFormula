6e620c754a6894a6ff03b9ae7943d12e29234093
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-06 10:02:40 +0200

    added payment constants

---
+++ SportBarFormula.Infastructure/Constants/DataConstants/PaymentConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace SportBarFormula.Infastructure.Constants.DataConstants;
+
+public static class PaymentConstants
+{
+    public const string PaymentDateStringFormat = "dd-MM-yyyy";
+    //[RegularExpression(@"^\d{2}-\d{2}-\d{4}$")]
+    //var isValid = DateTime.TryParseExact(model.PaymentDate, PaymentDateStringFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result);
+
+    
+    public const string PaymantAmountPrecision = "decimal(18,2)";
+
+    public const int PaymentMethodMinLength = 3; 
+    public const int PaymentMethodMaxLength = 50; 
+
+    public const int PaymentStatusMinLength = 3; 
+    public const int PaymentStatusMaxLength = 50; 
+}
