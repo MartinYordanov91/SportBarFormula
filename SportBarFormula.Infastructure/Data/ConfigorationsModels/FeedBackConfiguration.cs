e920006e0062351237a0e0b71b83817100bbc97e
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-06 10:38:45 +0200

    added  FeedBack configuration

---
+++ SportBarFormula.Infastructure/Data/ConfigorationsModels/FeedBackConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SportBarFormula.Infastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportBarFormula.Infastructure.Data.ConfigorationsModels
+{
+    public class FeedbackConfigurations : IEntityTypeConfiguration<Feedback>
+    {
+        public void Configure(EntityTypeBuilder<Feedback> builder)
+        {
+            builder.HasKey(f => f.FeedbackId);
+
+            builder.HasOne(f => f.User)
+                   .WithMany()
+                   .HasForeignKey(f => f.UserId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
