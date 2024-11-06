3ee9c7b177e7c7cfc2eac6a817cde1a15199e50d
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-06 10:03:08 +0200

    added payment configurations

---
+++ SportBarFormula.Infastructure/Data/ConfigorationsModels/PaymantConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportBarFormula.Infastructure.Data.Models;
+
+namespace SportBarFormula.Infastructure.Data.ConfigorationsModels;
+
+public class PaymentConfigurations : IEntityTypeConfiguration<Payment>
+{
+    public void Configure(EntityTypeBuilder<Payment> builder)
+    {
+        builder.HasKey(p => p.PaymentId);
+
+        builder.HasOne(p => p.Order)
+               .WithMany()
+               .HasForeignKey(p => p.OrderId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(p => p.User)
+               .WithMany()
+               .HasForeignKey(p => p.UserId)
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
