ecfc396e2bfe889a81d94a4c815017fda02c95bb
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-10 08:42:08 +0200

    adding a script to return the video to its initial position

---
+++ SportBarFormula/wwwroot/js/videoScript.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var iframe1 = document.getElementById('facebookVideo');
+    var iframe2 = document.getElementById('facebookVideo2');
+
+    function reloadIframe(iframe) {
+        var tempSrc = iframe.src;
+        iframe.src = '';
+        iframe.src = tempSrc;
+    }
+
+    iframe1.onload = function () {
+        setTimeout(function () {
+            reloadIframe(iframe1);
+        }, 27000); // Предполагаемото време за завършване на видеото в милисекунди
+    };
+
+    iframe2.onload = function () {
+        setTimeout(function () {
+            reloadIframe(iframe2);
+        }, 13000); // Предполагаемото време за завършване на видеото в милисекунди
+    };
+});
