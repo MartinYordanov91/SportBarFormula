1bb8ed825c979883cda9e0d167e14b36f9ffff61
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-11 09:59:17 +0200

    add MenuItemViewModel

---
+++ SportBarFormula.Core/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SportBarFormula.Core.ViewModels;
+
+public class MenuItemViewModel
+{
+    public required string Name { get; set; }
+
+    public string? Description { get; set; }
+    public required decimal Price { get; set; }
+
+    public required string Category { get; set; }
+
+    public required string ImageURL { get; set; }
+
+    public bool IsAvailable { get; set; } = true;
+
+    public string? Ingredients { get; set; }
+
+    public required int PreparationTime { get; set; }
+
+    public bool IsDeleted { get; set; } = false;
+}
