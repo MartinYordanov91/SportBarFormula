7f58b2ed653aa66694d05cc564e095c982f718b6
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-14 09:23:02 +0200

    more constants for MenuItem

--- SportBarFormula.Infastructure/Constants/DataConstants/MenuItemConstants.cs
+++ SportBarFormula.Infastructure/Constants/DataConstants/MenuItemConstants.cs
@@ -6,9 +6,11 @@ public static class MenuItemConstants
 {
     public const int MenuItemNameMinLength = 3;
     public const int MenuItemNameMaxLength = 150;
+    //[StringLength(MenuItemNameMaxLength, MinimumLength = MenuItemNameMinLength)]
 
     public const int MenuItemDescriptionMinLength = 0;
     public const int MenuItemDescriptionMaxLength = 500;
+    //[StringLength(MenuItemDescriptionMaxLength, MinimumLength = MenuItemDescriptionMinLength)]
 
     public const string MenuItemPriceMin = "0.00";
     public const string MenuItemPricenMax = "200.00";
@@ -17,12 +19,15 @@ public static class MenuItemConstants
 
     public const int MenuItemImageURLMinLength = 3;
     public const int MenuItemImageURLMaxLength = 500;
+    //[StringLength(MenuItemImageURLMaxLength, MinimumLength = MenuItemImageURLMinLength)]
 
-    public const int MenuItemQuantityMin = 0;
-    public const int MenuItemQuantityMax = 2000;
+    public const string MenuItemQuantityMin = "0";
+    public const string MenuItemQuantityMax = "2000";
+    //[Range(typeof(int), MenuItemQuantityMin, MenuItemQuantityMax)]
 
     public const int MenuItemIngredientsMinLength = 0;
     public const int MenuItemIngredientsMaxLength = 500;
+    //[StringLength(MenuItemIngredientsMaxLength, MinimumLength = MenuItemIngredientsMinLength)]
 
     public const string MenuItemPreparationTimeMin = "5";
     public const string MenuItemPreparationTimeMax = "30";
