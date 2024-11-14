3776f706ce83cc6ef184ce42e410f40c0f3de321
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-14 11:04:40 +0200

    added Category repository

---
+++ SportBarFormula.Infastructure/Repositorys/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportBarFormula.Infrastructure.Data;
+using SportBarFormula.Infrastructure.Data.Models;
+using SportBarFormula.Infrastructure.Repositorys.Contracts;
+
+namespace SportBarFormula.Infrastructure.Repositorys;
+
+public class CategoryRepository(SportBarFormulaDbContext context) : IRepository<Category>
+{
+    private readonly SportBarFormulaDbContext _context = context;
+
+    public async Task<IEnumerable<Category>> GetAllAsync()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+
+    public async Task<Category> GetByIdAsync(int id)
+    {
+        return await _context.Categories.FindAsync(id);
+    }
+
+    public async Task AddAsync(Category entity)
+    {
+        await _context.Categories.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Category entity)
+    {
+        _context.Categories.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await _context.Categories.FindAsync(id);
+
+        if (entity != null)
+        {
+            _context.Categories.Remove(entity);
+            await _context.SaveChangesAsync();
+        };
+    }
+
+}
