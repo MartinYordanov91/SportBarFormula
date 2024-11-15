5359b2f3b48f2b4891dd2a3ba49c2e921874b9d5
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-16 00:51:24 +0200

    added menuItem view models

---
+++ SportBarFormula.Core/ViewModels/MenuItem/MenuItemCardViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SportBarFormula.Core.ViewModels.MenuItem;
+
+public class MenuItemCardViewModel
+{
+    public int MenuItemId { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string ImageURL { get; set; }
+
+    public string? Ingredients { get; set; }
+
+    public required decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    public int CategoryId { get; set; }
+}
---
+++ SportBarFormula.Core/ViewModels/MenuItem/MenuItemListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SportBarFormula.Core.ViewModels.MenuItem;
+
+public class MenuItemListViewModel
+{
+    public IEnumerable<MenuItemCardViewModel> MenuItems { get; set; }
+    public IEnumerable<SelectListItem> Categories { get; set; }
+    public int? SelectedCategoryId { get; set; }
+}
