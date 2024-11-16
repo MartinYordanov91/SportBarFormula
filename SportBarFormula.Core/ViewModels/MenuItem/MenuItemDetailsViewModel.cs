046d20a4959c60bcdf11b4aea781627d56c67f43
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-17 01:21:50 +0200

    added view model for details

---
+++ SportBarFormula.Core/ViewModels/MenuItem/MenuItemDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SportBarFormula.Core.ViewModels.MenuItem;
+public class MenuItemDetailsViewModel
+{
+    public int MenuItemId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public int CategoryId { get; set; }
+    public string ImageURL { get; set; } = string.Empty;
+    public bool IsAvailable { get; set; }
+    public string Ingredients { get; set; } = string.Empty;
+    public int PreparationTime { get; set; }
+}
