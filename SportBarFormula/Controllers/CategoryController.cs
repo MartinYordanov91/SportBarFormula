5253f3d13339d2cad9b2823bd24e97055c5c80bb
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-17 22:45:30 +0200

    create CategoryController and added Index action

---
+++ SportBarFormula/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportBarFormula.Core.Services.Contracts;
+
+namespace SportBarFormula.Controllers;
+
+/// <summary>
+/// Manage categories where you can add, remove and edit categories for items.
+/// </summary>
+/// <param name="categoryService"></param>
+/// <param name="logger"></param>
+public class CategoryController(
+    ICategoryService categoryService,
+    ILogger<CategoryController> logger) : Controller
+{
+
+    private readonly ICategoryService _service = categoryService;
+    private readonly ILogger<CategoryController> _logger = logger;
+
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _service.GetAllCategoriesAsync();
+
+        return View(categories);
+    }
+
+
+}
