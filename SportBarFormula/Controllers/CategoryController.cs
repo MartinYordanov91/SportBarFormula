081dd01c8e69e5d9d7e5822d7e86a56dbbc57903
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-17 23:24:47 +0200

    An action has been created which is responsible for creating a new category

--- SportBarFormula/Controllers/CategoryController.cs
+++ SportBarFormula/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using SportBarFormula.Core.Services.Contracts;
+using SportBarFormula.Core.ViewModels.Category;
 
 namespace SportBarFormula.Controllers;
 
@@ -16,13 +18,46 @@ public class CategoryController(
     private readonly ICategoryService _service = categoryService;
     private readonly ILogger<CategoryController> _logger = logger;
 
-
+    //----------------------------------------------------------------------------------------------------> Index
+    /// <summary>
+    /// Retrieves and displays all categories.
+    /// </summary>
+    /// <returns>View with all categories.</returns>
     [HttpGet]
     public async Task<IActionResult> Index()
     {
         var categories = await _service.GetAllCategoriesAsync();
 
         return View(categories);
+    }
+
+    //----------------------------------------------------------------------------------------------------> Create
+    /// <summary>
+    /// Displays a form to create a new category.
+    /// </summary>
+    /// <returns>View to create a new category.</returns>
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    /// <summary>
+    /// Processes the post request to create a new category.
+    /// </summary>
+    /// <param name="model">ViewModel for the new category.</param>
+    /// <returns>Redirects to the Index view on successful creation or displays the form again on failure.</returns>
+    [HttpPost]
+    public async Task<IActionResult> Create(CategoryViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        await _service.AddCategoryAsync(model);
+
+        return RedirectToAction(nameof(Index));
     }
 
 
