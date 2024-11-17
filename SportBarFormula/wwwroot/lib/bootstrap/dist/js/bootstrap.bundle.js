b744b5c8f20e44d4f0829679addb863de6ed09c7
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-18 01:19:53 +0200

    changing the method of obtaining consent

--- SportBarFormula/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
+++ SportBarFormula/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
@@ -570,15 +570,17 @@
         defaultPrevented = jQueryEvent.isDefaultPrevented();
       }
 
-      if (isNative) {
-        evt = document.createEvent('HTMLEvents');
-        evt.initEvent(typeEvent, bubbles, true);
-      } else {
-        evt = new CustomEvent(event, {
-          bubbles,
-          cancelable: true
-        });
-      } // merge custom information in our event
+        if (isNative) {
+            evt = new Event(typeEvent, {
+                bubbles: bubbles,
+                cancelable: true
+            });
+        } else {
+            evt = new CustomEvent(event, {
+                bubbles: bubbles,
+                cancelable: true
+            });
+        } // merge custom information in our event
 
 
       if (typeof args !== 'undefined') {
