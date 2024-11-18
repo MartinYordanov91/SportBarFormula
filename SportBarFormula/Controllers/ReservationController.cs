f1c5099e7ca1f83b070c29c4d6063d3bdc8498e6
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-18 21:13:51 +0200

    adding a MyReservation action in the controller

--- SportBarFormula/Controllers/ReservationController.cs
+++ SportBarFormula/Controllers/ReservationController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportBarFormula.Core.Services.Contracts;
+using SportBarFormula.Core.ViewModels.Reservation;
+using System.Security.Claims;
 
 namespace SportBarFormula.Controllers;
 
@@ -15,7 +17,7 @@ public class ReservationController(
     private readonly IReservationService _service = service;
     private readonly ILogger<ReservationController> _logger = logger;
 
-
+    //--------------------------------------------------------------------------------------------------------> Index
     /// <summary>
     /// List of all reservations for admins.
     /// </summary>
@@ -25,4 +27,19 @@ public class ReservationController(
         var reservations = await _service.GetAllReservationsAsync();
         return View(reservations);
     }
+
+    //--------------------------------------------------------------------------------------------------------> MyReservations
+    /// <summary>
+    /// Shows customers their current and past bookings.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> MyReservations()
+    {
+        string userId = this.User.Id();
+        var reservations = await _service.GetReservationsByUserIdAsync(userId);
+
+        return View(reservations);
+    }
+
 }
