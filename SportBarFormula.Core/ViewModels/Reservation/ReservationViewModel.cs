84a8a613686e768e3df1d3996cde48e619192ed3
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-18 17:50:10 +0200

    getting reservations view model to work with

---
+++ SportBarFormula.Core/ViewModels/Reservation/ReservationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportBarFormula.Core.ViewModels.Reservation;
+
+public class ReservationViewModel
+{
+    public int ReservationId { get; set; }
+
+    public required string UserId { get; set; }
+
+    [Display(Name = "Дата на резервация")]
+    [RegularExpression(@"^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$")]
+    public required DateTime ReservationDate { get; set; }
+
+    [Display(Name = "Маса")]
+    public int TableId { get; set; }
+
+    [Display(Name = "Брой гости")]
+    public int NumberOfGuests { get; set; }
+
+    public bool IsCanceled { get; set; } = false;
+}
