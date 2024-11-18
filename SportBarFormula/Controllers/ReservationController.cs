b142e7ca40e9d4300d3a7b8e59c66000e2a70f8b
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-18 19:22:44 +0200

    adding yes reservations controller and action index

---
+++ SportBarFormula/Controllers/ReservationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportBarFormula.Core.Services.Contracts;
+
+namespace SportBarFormula.Controllers;
+
+/// <summary>
+/// Booking Management Controller.
+/// </summary>
+public class ReservationController(
+    ILogger<ReservationController> logger,
+    IReservationService service
+    ) : Controller
+{
+
+    private readonly IReservationService _service = service;
+    private readonly ILogger<ReservationController> _logger = logger;
+
+
+    /// <summary>
+    /// List of all reservations for admins.
+    /// </summary>
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var reservations = await _service.GetAllReservationsAsync();
+        return View(reservations);
+    }
+}
