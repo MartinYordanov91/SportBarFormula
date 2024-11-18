a7704c1c9bbeecf1f6f24bd3e11d72f1bcc905c8
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-18 21:20:31 +0200

    add method GetAllReservationsAsync in the service

--- SportBarFormula.Core/Services/Contracts/IReservationService.cs
+++ SportBarFormula.Core/Services/Contracts/IReservationService.cs
@@ -1,9 +1,12 @@
 ﻿using SportBarFormula.Core.ViewModels.Reservation;
 using SportBarFormula.Infrastructure.Repositorys.Contracts;
+using System.Threading.Tasks;
 
 namespace SportBarFormula.Core.Services.Contracts;
 
 public interface IReservationService
 {
     Task<IEnumerable<ReservationViewModel>> GetAllReservationsAsync();
+
+    Task<IEnumerable<ReservationViewModel>> GetReservationsByUserIdAsync(string userId);
 }
--- SportBarFormula.Core/Services/ReservationService.cs
+++ SportBarFormula.Core/Services/ReservationService.cs
@@ -1,9 +1,11 @@
-﻿using SportBarFormula.Core.Services.Contracts;
+﻿using Microsoft.AspNetCore.Mvc;
+using SportBarFormula.Core.Services.Contracts;
 using SportBarFormula.Core.ViewModels.Reservation;
 using SportBarFormula.Infrastructure.Data.Models;
 using SportBarFormula.Infrastructure.Repositorys.Contracts;
 using System.Globalization;
 using static SportBarFormula.Infrastructure.Constants.DataConstants.ReservationConstants;
+
 
 namespace SportBarFormula.Core.Services;
 
@@ -14,7 +16,6 @@ public class ReservationService(IRepository<Reservation> repository) : IReservat
 {
     private readonly IRepository<Reservation> _repository = repository;
 
-
     /// <summary>
     /// Returns all reservations.
     /// </summary>
@@ -22,14 +23,40 @@ public class ReservationService(IRepository<Reservation> repository) : IReservat
     public async Task<IEnumerable<ReservationViewModel>> GetAllReservationsAsync()
     {
         var reservations = await _repository.GetAllAsync();
-        return reservations.Select(r => new ReservationViewModel
-        {
-            ReservationId = r.ReservationId,
-            UserId = r.UserId,
-            ReservationDate = r.ReservationDate.ToString(ReservationDateTimeStringFormat, CultureInfo.InvariantCulture),
-            TableId = r.TableId,
-            NumberOfGuests = r.NumberOfGuests,
-            IsCanceled = r.IsCanceled
-        });
+        return reservations
+            .Select(r => new ReservationViewModel
+            {
+                ReservationId = r.ReservationId,
+                UserId = r.UserId,
+                ReservationDate = r.ReservationDate.ToString(ReservationDateTimeStringFormat, CultureInfo.InvariantCulture),
+                TableId = r.TableId,
+                NumberOfGuests = r.NumberOfGuests,
+                IsCanceled = r.IsCanceled
+            })
+            .ToList();
     }
+
+    /// <summary> 
+    /// Returns reservations for a specific user.
+    /// </summary> 
+    /// <param name="userId">The ID of the user.</param>
+    /// <returns>A list of reservations for the user.</returns>
+    public async Task<IEnumerable<ReservationViewModel>> GetReservationsByUserIdAsync(string userId)
+    {
+        var reservations = await _repository.GetAllAsync();
+
+        return reservations
+            .Where(r => r.UserId == userId)
+            .Select(r => new ReservationViewModel
+            {
+                ReservationId = r.ReservationId,
+                UserId = r.UserId,
+                ReservationDate = r.ReservationDate.ToString(ReservationDateTimeStringFormat, CultureInfo.InvariantCulture),
+                TableId = r.TableId,
+                NumberOfGuests = r.NumberOfGuests,
+                IsCanceled = r.IsCanceled
+            })
+            .ToList();
+    }
+
 }
