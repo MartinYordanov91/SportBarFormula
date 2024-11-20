8c90d438f316904163a645c4fd1a6efa92ce7908
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-20 10:55:53 +0200

    added tableSeeds

---
+++ SportBarFormula.Infastructure/Data/SeedData/TableSeeds.cs
@@ -0,0 +1,99 @@
+﻿using SportBarFormula.Infrastructure.Data.Models;
+
+namespace SportBarFormula.Infrastructure.Data.SeedData;
+
+public static class TableSeeds
+{
+    public static IEnumerable<Table> GenerateTables()
+    {
+        return new List<Table>()
+        {
+            new Table()
+            {
+                TableId = 1,
+                Capacity = 6,
+                TableNumber = "100",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+
+            new Table()
+            {
+                TableId = 2,
+                Capacity = 6,
+                TableNumber = "101",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+
+            new Table()
+            {
+                TableId = 3,
+                Capacity = 6,
+                TableNumber = "102",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+
+
+            new Table()
+            {
+                TableId = 4,
+                Capacity = 6,
+                TableNumber = "103",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+
+
+            new Table()
+            {
+                TableId = 5,
+                Capacity = 6,
+                TableNumber = "104",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+
+
+            new Table()
+            {
+                TableId = 6,
+                Capacity = 6,
+                TableNumber = "105",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+
+
+            new Table()
+            {
+                TableId = 7,
+                Capacity = 6,
+                TableNumber = "106",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+
+
+            new Table()
+            {
+                TableId = 8,
+                Capacity = 4,
+                TableNumber = "107",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+
+
+            new Table()
+            {
+                TableId = 9,
+                Capacity = 4,
+                TableNumber = "108",
+                Location ="indoor",
+                IsAvailable = true,
+            },
+        };
+    }
+}
