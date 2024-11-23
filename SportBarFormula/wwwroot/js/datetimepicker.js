5d942d8a91944edfe7c0a25b5195cd4f2fe2583f
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-11-23 08:46:25 +0200

    feat(js): add date formatting and reservation size warning in form
     - Added JavaScript code to format date and time fields in reservation form.
     - Implemented warning message when number of guests exceeds 10.
     - Utilized jQuery for dynamic form updates and validations.
     - Ensured compatibility with Bootstrap dropdown menu initialization.

---
+++ SportBarFormula/wwwroot/js/datetimepicker.js
@@ -0,0 +1,24 @@
+﻿$(function ($) {
+    // Комбиниране на стойностите от селект листовете за дата и час в едно поле
+    $('.date-part').on('change', function () {
+        var day = $('#Day').val().padStart(2, '0');
+        var month = $('#Month').val().padStart(2, '0');
+        var year = $('#Year').val();
+        var hour = $('#Hour').val().padStart(2, '0');
+        var minute = $('#Minute').val().padStart(2, '0');
+
+        var formattedDate = `${day}-${month}-${year} ${hour}:${minute}`;
+        $('input[name="ReservationDate"]').val(formattedDate);
+    });
+
+    // Инициализиране на Bootstrap dropdown менюто
+    $('.dropdown-toggle').dropdown();
+
+    $('#NumberOfGuests').on('input', function () {
+        var numGuests = $(this).val();
+        if (numGuests > 10) {
+            alert('За резервация за повече от 10 човека, моля свържете се с нас.');
+            $(this).val('');
+        }
+    });
+});
