d940e9ffc28ccb34634c965e9ad4d3e0facd79e4
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-12-02 17:00:02 +0200

    feat(ViewModel): add ManageRolesViewModel for Admin area
    - Added ManageRolesViewModel to manage roles for users in the Admin area.
    - Included properties for UserId, UserName, UserRoles, and AllRoles.
    - Ensured the model provides necessary data for role management functionality.

---
+++ SportBarFormula.Core/ViewModels/Admin/ManageRolesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SportBarFormula.Core.ViewModels.Admin;
+
+public class ManageRolesViewModel
+{
+    public string UserId { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public IEnumerable<string> UserRoles { get; set; } = null!;
+    public IEnumerable<IdentityRole> AllRoles { get; set; } = null!;
+}
