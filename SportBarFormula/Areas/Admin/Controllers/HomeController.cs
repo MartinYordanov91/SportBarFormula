a43200693860d9486bc7a9fea4cac49bfd73ea0b
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-12-02 16:37:41 +0200

    feat(Controller): add new controller for Admin Home
    - Added HomeController for the Admin area.
    - Includes an Index method to display the admin home page.
    - Ensures access control with authorization for Admin roles only.
    - Added XML comments for better code documentation.

---
+++ SportBarFormula/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportBarFormula.Areas.Admin.Controllers;
+
+/// <summary>
+/// Controller for handling the home page in the Admin area.
+/// </summary>
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class HomeController : Controller
+{
+    /// <summary>
+    /// Displays the home page.
+    /// </summary>
+    /// <returns>A view for the home page.</returns>
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
