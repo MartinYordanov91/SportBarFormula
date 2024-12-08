980e7a531ff4ed76e3edc6b3d0f8a6db34e1e29f
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-12-08 23:38:02 +0200

    Added custom error handling and error views for status codes 400 and 401

--- SportBarFormula/Controllers/HomeController.cs
+++ SportBarFormula/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SportBarFormula.Core.ViewModels;
 using System.Diagnostics;
@@ -27,13 +28,25 @@ public class HomeController() : Controller
         return View();
     }
 
+   
     /// <summary>
     /// Handles errors and returns the Error view.
     /// </summary>
     /// <returns>Returns the Error view with the error details.</returns>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult Error(int statusCode)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        if(statusCode == 400)
+        {
+            return View("Error400");
+        }
+
+        if(statusCode == 401)
+        {
+            return View("Error401");
+        }
+
+        return View();
     }
 }
